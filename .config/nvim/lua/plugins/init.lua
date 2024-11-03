b7b1fd2cf41652006f3a72d530f768ca5cea39a4
Mikk Vahaste <mikk.vahaste@gmail.com>
2024-11-03 21:30:58 +0000

    Added better commenting

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -85,4 +85,24 @@ return {
     lazy = true,
     event = { "BufReadPost" },
   },
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    lazy = true,
+    event = { "BufReadPost" },
+    config = function()
+      require("ts_context_commentstring").setup {
+        enable_autocmd = false,
+      }
+    end,
+  },
+  {
+    "numToStr/Comment.nvim",
+    lazy = true,
+    event = { "BufReadPost" },
+    config = function()
+      require("Comment").setup {
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      }
+    end,
+  },
 }
