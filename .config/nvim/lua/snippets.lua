79f5e900b8929539a20109cba8b95daa37df7baf
Mikk Vahaste <mikk.vahaste@gmail.com>
2024-11-04 17:31:24 +0000

    Created snippets.lua

---
+++ .config/nvim/lua/snippets.lua
@@ -0,0 +1,37 @@
+local prettierrc = [[
+{
+  "arrowParens": "avoid",
+  "bracketSpacing": true,
+  "htmlWhitespaceSensitivity": "css",
+  "insertPragma": false,
+  "jsxSingleQuote": false,
+  "printWidth": 80,
+  "proseWrap": "always",
+  "quoteProps": "as-needed",
+  "requirePragma": false,
+  "semi": false,
+  "singleQuote": false,
+  "tabWidth": 2,
+  "trailingComma": "all",
+  "useTabs": false
+}
+]]
+
+function WritePrettierrc()
+  local output_file = ".prettierrc"
+  local file = io.open(output_file, "w")
+
+  if file then
+    file:write(prettierrc)
+    file:close()
+    print(".prettierrc created in project root")
+  else
+    print("Error: Could not create .prettierrc")
+  end
+end
+
+vim.api.nvim_create_user_command(
+  "CreatePrettierrc",
+  WritePrettierrc,
+  { desc = "Creates a base .prettierrc file in the project root" }
+)
