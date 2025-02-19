1b566072c16046f2a1477bef689844c6c2577705
mvahaste <mvahaste@proton.me>
2025-02-19 21:02:41 +0200

    Removed harpoon buffer switching shortcuts due to conflict

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -28,9 +28,6 @@ vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "
 -- vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
 --   { desc = "Toggle Harpoon list" })
 
-vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end, { desc = "Switch to Harpoon buffer 1" })
-vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end, { desc = "Switch to Harpoon buffer 2" })
-vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end, { desc = "Switch to Harpoon buffer 3" })
-vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end, { desc = "Switch to Harpoon buffer 4" })
+-- vim.keymap.set("n", "<C-u>", function() harpoon:list():select(1) end, { desc = "Switch to Harpoon buffer 1" })
 
 return {}
