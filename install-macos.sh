45a0d8e71483b70b313bf7b7759895b46b1785e5
mvahaste <mvahaste@proton.me>
2025-03-27 17:30:12 +0200

    Installed LazyVim, fd, updated email in macOS install script

--- install-macos.sh
+++ install-macos.sh
@@ -4,7 +4,7 @@
 git clone --depth 1 https://github.com/mvahaste/dotfiles.git ~/dotfiles
 
 # Install dependencies
-brew install stow zsh tmux unzip ripgrep gh nodejs npm neovim lazygit zoxide fzf glow pngpaste black maven
+brew install stow zsh tmux unzip ripgrep gh nodejs npm neovim lazygit zoxide fzf glow pngpaste black maven fd
 
 # Set up global npm directory
 mkdir -p ~/.npm-global
@@ -18,7 +18,7 @@ curl -fsSL https://deno.land/install.sh | sh
 
 # Set git name and email
 if [ $USER = "mvahaste" ]; then
-  git config --global user.email "mikk.vahaste@gmail.com"
+  git config --global user.email "mvahaste@proton.me"
   git config --global user.name "Mikk Vahaste"
 fi
 
