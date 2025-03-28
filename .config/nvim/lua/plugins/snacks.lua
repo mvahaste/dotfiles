4179acf9648b9164f4be0c01655b62f39f762433
mvahaste <mvahaste@proton.me>
2025-03-29 00:28:21 +0200

    Added snacks, removed lazygit

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,30 @@
+return {
+  "folke/snacks.nvim",
+  friority = 1000,
+  lazy = false,
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    lazygit = { enabled = true },
+    gitbrowse = { enabled = true },
+    bigfile = { enabled = true },
+    -- dashboard = { enabled = true },
+    -- explorer = { enabled = true },
+    -- indent = { enabled = true },
+    input = { enabled = true },
+    -- picker = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    scope = { enabled = true },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+  },
+  config = function()
+    ---@diagnostic disable-next-line: undefined-global
+    vim.keymap.set("n", "<leader>lg", function() Snacks.lazygit.open(opts) end, { desc = "Open Lazygit" })
+    ---@diagnostic disable-next-line: undefined-global
+    vim.keymap.set("n", "<leader>gb", function() Snacks.gitbrowse() end, { desc = "Gitbrowse" })
+  end
+}
