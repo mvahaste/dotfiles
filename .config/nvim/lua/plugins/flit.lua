bed395d8a367b48def5df94456600ec4d7e383ba
mvahaste <mvahaste@proton.me>
2025-03-30 23:49:31 +0300

    Added flit

---
+++ .config/nvim/lua/plugins/flit.lua
@@ -0,0 +1,22 @@
+return {
+  "ggandor/flit.nvim",
+  lazy = true,
+  event = "BufRead",
+  depenencies = {
+    "leap.nvim",
+    "repeat.vim",
+  },
+  config = function()
+    require('flit').setup {
+      keys = { f = 'f', F = 'F', t = 't', T = 'T' },
+      -- A string like "nv", "nvo", "o", etc.
+      labeled_modes = "v",
+      -- Repeat with the trigger key itself.
+      clever_repeat = true,
+      multiline = true,
+      -- Like `leap`s similar argument (call-specific overrides).
+      -- E.g.: opts = { equivalence_classes = {} }
+      opts = {}
+    }
+  end,
+}
