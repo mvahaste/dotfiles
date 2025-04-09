40ed449b27ec00cda6d7502210aa70b172540a57
mvahaste <mvahaste@proton.me>
2025-04-09 12:20:32 +0300

    Enable copilot for all files except .env

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -8,6 +8,16 @@ return {
       suggestion = {
         auto_trigger = true,
       },
+      filetypes = {
+        ["*"] = true,
+        sh = function()
+          if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), "^%.env.*") then
+            -- disable for .env files
+            return false
+          end
+          return true
+        end,
+      }
     }
     vim.keymap.set("i", "<M-i>", function() require("copilot.suggestion").accept() end,
       { noremap = true, silent = true, desc = "Accept Copilot suggestion" })
