f45e34146e7379c0da63686e4ba9526c0c487f88
mvahaste <mvahaste@proton.me>
2025-05-13 20:49:17 +0300

    Updated vimtex settings

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -5,5 +5,25 @@ return {
   init = function()
     -- VimTeX configuration goes here, e.g.
     vim.g.vimtex_view_method = "skim"
+    -- map <leader>lc to compile
+    vim.keymap.set("n", "<leader>lc", "<Plug>(vimtex-compile)", { desc = "Compile LaTeX" })
+    -- clean up files after compilation
+    vim.g.vimtex_compiler_clean_paths = {
+      "aux",
+      "bbl",
+      "bcf",
+      "blg",
+      "idx",
+      "ilg",
+      "ind",
+      "lof",
+      "log",
+      "out",
+      "pdfsync",
+      "run.xml",
+      "toc",
+    }
+    -- don't open/close automatically
+    vim.g.vimtex_quickfix_mode = 0
   end,
 }
