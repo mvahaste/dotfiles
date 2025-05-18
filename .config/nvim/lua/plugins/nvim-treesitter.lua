0db863a61bc8ac9a55efb733b094f526ebdf9e20
mvahaste <mvahaste@proton.me>
2025-05-18 10:37:34 +0300

    fix: broken treesitter highlighting

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -2,33 +2,22 @@ return {
   "nvim-treesitter/nvim-treesitter",
   lazy = false,
   build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup {
-      highlight = {
-        disable = { "latex" },
-      },
-    }
-  end,
   opts = {
     ensure_installed = {
       "vim",
-      "lua",
       "vimdoc",
+      "bash",
+      "lua",
+      "python",
       "html",
       "css",
+      "json",
+      "toml",
       "javascript",
       "typescript",
       "tsx",
-      "vue",
       "markdown",
       "markdown_inline",
-      "json",
-      "toml",
-      "latex",
-      "bash",
-      "python",
-      "cpp",
-      "typst",
     },
   },
 }
