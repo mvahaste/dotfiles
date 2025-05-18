409c4e249b8d518cf9ecb9481b5c98405d9b6455
mvahaste <mvahaste@proton.me>
2025-05-18 12:49:24 +0300

    feat: eslint lsp

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "jsonls", "ts_ls", "tailwindcss", "taplo", "pyright" }
+local servers = { "html", "cssls", "jsonls", "ts_ls", "eslint", "tailwindcss", "taplo", "pyright" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
